359872dab55ca58856c6ed28ef1ec3b3664bf8d1
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-02-17 13:31:00 +0100

    Bug with CUDA

--- datasets/masked_dataset.py
+++ datasets/masked_dataset.py
@@ -18,7 +18,7 @@ class MaskedDataset(torch.utils.data.Dataset):
     def __getitem__(self, item):
         gt, annotations, info = self.data[item]
         masks = self.masks.get_item(10, (gt.size(2), gt.size(3)), gt.size(1)) if self.masks else annotations
-        masked_frames = (1 - masks) * gt + masks*torch.FloatTensor([0.485, 0.456, 0.406]).view(3, 1, 1, 1)
+        masked_frames = (1 - masks) * gt + masks*torch.FloatTensor([0.485, 0.456, 0.406], device=gt.device).view(3, 1, 1, 1)
         return (masked_frames, masks), gt, info
 
     def __len__(self):
