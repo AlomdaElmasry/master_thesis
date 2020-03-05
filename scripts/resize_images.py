950b57ec3f3b2be80cf4dd03a2b9b4abffda6d6a
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-03-05 16:35:17 +0100

    Modified resizing script

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -8,7 +8,11 @@ import progressbar
 parser = argparse.ArgumentParser(description='Cleans invalid images')
 parser.add_argument('--data-path', required=True, help='Path where the images are stored')
 parser.add_argument('--destination-path', required=True, help='Destination path where the will be stored')
+parser.add_argument('--max-width', type=int, default=910, help='Number of workers to use')
+parser.add_argument('--max-height', type=int, default=480, help='Number of workers to use')
+parser.add_argument('--keep-ratio', type=bool, default=True, help='Number of workers to use')
 parser.add_argument('--max-workers', type=int, default=10, help='Number of workers to use')
+
 args = parser.parse_args()
 
 
@@ -23,10 +27,18 @@ def handle_folder(folder_path, args, bar, i):
         if os.path.isfile(file_path):
             new_file_path = file_path.replace(folder_path, new_folder_path)
             file_extension = os.path.splitext(folder_item)[-1].replace('.', '')
+            saving_flags = [int(cv2.IMWRITE_JPEG_QUALITY), 50] if file_extension == 'jpg' else []
             if file_extension in ['jpg', 'png']:
                 image = cv2.imread(os.path.join(folder_path, folder_item))
-                image_resized = cv2.resize(image, (256, 256))
-                cv2.imwrite(new_file_path, image_resized)
+                if image.shape[0] > args.max_height or image.shape[1] > args.max_width:
+                    new_height = args.max_height if image.shape[0] >= image.shape[1] else \
+                        round(image.shape[0] * args.max_width / image.shape[1])
+                    new_width = args.max_width if image.shape[1] > image.shape[0] else \
+                        round(image.shape[1] * args.max_height / image.shape[0])
+                    new_height = new_height if args.keep_ratio else args.max_height
+                    new_width = new_width if args.keep_ratio else args.max_width
+                    image = cv2.resize(image, (new_width, new_height))
+                cv2.imwrite(new_file_path, image, saving_flags)
             else:
                 shutil.copy(file_path, new_file_path)
     # Update bar counter
