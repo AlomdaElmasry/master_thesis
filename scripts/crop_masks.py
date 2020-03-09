fd9de21d841ac0b24c6fef1af16d61aeece11d40
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-03-09 14:05:02 +0100

    LEVEL COMP

--- scripts/crop_masks.py
+++ scripts/crop_masks.py
@@ -9,7 +9,7 @@ import shutil
 parser = argparse.ArgumentParser(description='Crops mask to the visual')
 parser.add_argument('--data-path', required=True, help='Path where the images are stored')
 parser.add_argument('--destination-path', required=True, help='Whether or not to remove entire sequence')
-parser.add_argument('--formats', nargs='+', default=['jpg', 'jpeg', 'png'], help='Image formats to search in the path')
+parser.add_argument('--compression-level', type=int, default=9, help='Compression level of PNG images')
 parser.add_argument('--max-workers', type=int, default=10, help='Number of workers to use')
 args = parser.parse_args()
 
@@ -18,7 +18,7 @@ def crop_image(image):
     return image[np.ix_(image.any(1), image.any(0))]
 
 
-def crop_sequence(sequence_path, bar, i):
+def crop_sequence(sequence_path, args, bar, i):
     new_folder_path = folder_path.replace(args.data_path, args.destination_path)
     if not os.path.exists(new_folder_path):
         os.makedirs(new_folder_path)
@@ -28,6 +28,7 @@ def crop_sequence(sequence_path, bar, i):
         if os.path.isfile(file_path):
             new_file_path = file_path.replace(folder_path, new_folder_path)
             file_extension = os.path.splitext(folder_item)[-1].replace('.', '')
+            saving_flags = [cv2.IMWRITE_PNG_COMPRESSION, args.compression_level]
             if file_extension in ['png']:
                 image = (cv2.imread(os.path.join(folder_path, folder_item), cv2.IMREAD_GRAYSCALE) > 0) * 255
                 image_croped = crop_image(image)
@@ -53,5 +54,5 @@ bar = progressbar.ProgressBar(max_value=len(folder_paths))
 
 # Walk through the folders of args.data_path
 for i, folder_path in enumerate(folder_paths):
-    crop_sequence(folder_path, bar, i)
+    crop_sequence(folder_path, args, bar, i)
     # executor.submit(crop_sequence, folder_path, bar, i)
