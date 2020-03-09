c203845e881359c63ff01fc84e76beb683a1ae8f
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-03-09 14:00:04 +0100

    Binarized masks during croping

--- scripts/crop_masks.py
+++ scripts/crop_masks.py
@@ -14,7 +14,7 @@ parser.add_argument('--max-workers', type=int, default=10, help='Number of worke
 args = parser.parse_args()
 
 
-def crop_image(image, tol=0):
+def crop_image(image):
     return image[np.ix_(image.any(1), image.any(0))]
 
 
@@ -29,14 +29,14 @@ def crop_sequence(sequence_path, bar, i):
             new_file_path = file_path.replace(folder_path, new_folder_path)
             file_extension = os.path.splitext(folder_item)[-1].replace('.', '')
             if file_extension in ['png']:
-                image = cv2.imread(os.path.join(folder_path, folder_item), cv2.IMREAD_GRAYSCALE)
+                image = (cv2.imread(os.path.join(folder_path, folder_item), cv2.IMREAD_GRAYSCALE) > 0) * 255
                 image_croped = crop_image(image)
                 if image_croped.shape != (0, 0):
                     cv2.imwrite(new_file_path, image_croped)
                 else:
                     print('Image {} has no mask in it'.format(file_path))
             else:
-                pass # shutil.copy(file_path, new_file_path)
+                pass  # shutil.copy(file_path, new_file_path)
     # Update bar counter
     if bar.value < i:
         bar.update(i)
@@ -54,4 +54,4 @@ bar = progressbar.ProgressBar(max_value=len(folder_paths))
 # Walk through the folders of args.data_path
 for i, folder_path in enumerate(folder_paths):
     crop_sequence(folder_path, bar, i)
-    #executor.submit(crop_sequence, folder_path, bar, i)
+    # executor.submit(crop_sequence, folder_path, bar, i)
