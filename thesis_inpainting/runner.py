a82f720ce40a431dc25cbb9a381213fabad3e266
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-06-29 00:07:10 +0200

    Bug

--- thesis_inpainting/runner.py
+++ thesis_inpainting/runner.py
@@ -88,7 +88,7 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         for t in range(x.size(1)):
             self.logger.info('Step {}/{}'.format(t, x.size(1)))
             x_target, m_target, y_target = x[:, t].unsqueeze(0), m[:, t].unsqueeze(0), y[:, t].unsqueeze(0)
-            t_candidates = ThesisInpaintingRunner.compute_priority_indexes(t, x.size(1), d_step=2, max_d=4)
+            t_candidates = ThesisInpaintingRunner.compute_priority_indexes(t, x.size(1), d_step=5, max_d=30)
             while len(t_candidates) > 0 and torch.sum(m_target) * 100 / m_target.numel() > 1:
                 r_index = [t_candidates.pop(0)]
                 x_ref, m_ref = x[:, r_index].unsqueeze(0), m[:, r_index].unsqueeze(0)
