833455c9b8a3c870525b6c32523172f5796f8675
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-07-16 23:35:27 +0200

    BUG

--- thesis_inpainting/runner.py
+++ thesis_inpainting/runner.py
@@ -113,7 +113,6 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         fill_color = torch.as_tensor([0.485, 0.456, 0.406], dtype=torch.float32).view(1, 3, 1, 1).to(x.device)
         y_inpainted = torch.zeros_like(x)
         for t in range(x.size(1)):
-            # self.logger.info('Step {}/{}'.format(t, x.size(1)))
             x_target, m_target, y_target, y_hat = x[:, t].unsqueeze(0), m[:, t].unsqueeze(0), y[:, t].unsqueeze(0), None
             t_candidates = ThesisInpaintingRunner.compute_priority_indexes(t, x.size(1), d_step=2, max_d=10)
             while (len(t_candidates) > 0 and torch.sum(m_target) * 100 / m_target.numel() > 1) or y_hat is None:
@@ -135,7 +134,7 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         y_inpainted = torch.zeros_like(x)
         for t in range(x.size(1)):
             x_target, m_target, y_target, y_hat = x[:, t].unsqueeze(0), m[:, t].unsqueeze(0), y[:, t].unsqueeze(0), None
-            t_candidates = ThesisInpaintingRunner.compute_priority_indexes(t, x.size(1), d_step=3, max_d=9E9)
+            t_candidates = ThesisInpaintingRunner.compute_priority_indexes(t, x.size(1), d_step=2, max_d=10)
             while (len(t_candidates) > 0 and torch.sum(m_target) * 100 / m_target.numel() > 1) or y_hat is None:
                 r_index = [t_candidates.pop(0)]
                 x_ref, m_ref = x[:, r_index].unsqueeze(0), m[:, r_index].unsqueeze(0)
