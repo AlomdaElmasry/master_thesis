0c35236ef737ce304320247b2b533a27845f2e1a
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-07-30 23:43:38 +0200

    Modified

--- thesis_inpainting/runner.py
+++ thesis_inpainting/runner.py
@@ -167,7 +167,7 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         )
 
     @staticmethod
-    def inpainting_algorithm_ff(x, m, model_alignment, model, s=1, D=20, e=1):
+    def inpainting_algorithm_ff(x, m, model_alignment, model, s=2, D=20, e=1):
         fill_color = torch.as_tensor([0.485, 0.456, 0.406], dtype=torch.float32).view(1, 3, 1, 1).to(x.device)
         y_inpainted = torch.zeros_like(x)
         for t in range(x.size(1)):
@@ -195,7 +195,7 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         )
 
     @staticmethod
-    def inpainting_algorithm_ip(x, m, model_alignment, model, s=1, D=20, e=1):
+    def inpainting_algorithm_ip(x, m, model_alignment, model, s=2, D=20, e=1):
         fill_color = torch.as_tensor([0.485, 0.456, 0.406], dtype=torch.float32).view(1, 3, 1, 1).to(x.device)
         y_inpainted, m_inpainted = x.unsqueeze(0), m.unsqueeze(0)
         t_list = sorted(list(range(x.size(1))), key=lambda xi: abs(xi - x.size(1) // 2))
@@ -224,7 +224,7 @@ class ThesisInpaintingRunner(thesis.runner.ThesisRunner):
         return t_list_inpainted + t_list_ff
 
     @staticmethod
-    def inpainting_algorithm_cp(x, m, model_alignment, model, N=20, s=1, e=1):
+    def inpainting_algorithm_cp(x, m, model_alignment, model, N=20, s=2, e=1):
         fill_color = torch.as_tensor([0.485, 0.456, 0.406], dtype=torch.float32).view(1, 3, 1, 1).to(x.device)
         y_inpainted, m_inpainted = x.unsqueeze(0), m.unsqueeze(0)
         for i in range(N):
